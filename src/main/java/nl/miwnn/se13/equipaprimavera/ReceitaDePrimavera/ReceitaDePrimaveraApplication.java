e78ed7d21cc2658432d5393932489612a55e0382
Bram van Ham <bramvanham1994@hotmail.com>
2024-04-08 13:29:00 +0200

    First commit

---
+++ src/main/java/nl/miwnn/se13/equipaprimavera/ReceitaDePrimavera/ReceitaDePrimaveraApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se13.equipaprimavera.ReceitaDePrimavera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaDePrimaveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitaDePrimaveraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se13/equipaprimavera/ReceitaDePrimavera/ReceitaDePrimaveraApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se13.equipaprimavera.ReceitaDePrimavera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaDePrimaveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
