9cf85ab2fc215ba4fe3103057aa78761321164a2
mirjam schmitz <mirjam_schmitz@hotmail.com>
2024-04-18 15:25:17 +0200

    small change

---
+++ src/test/java/nl/miwnn/se13/equipaprimavera/ReceitaDePrimavera/ExampleTestTest.java
@@ -0,0 +1,16 @@
+package nl.miwnn.se13.equipaprimavera.ReceitaDePrimavera;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Mirjam Schmitz
+ * <p>
+ * Purpose for the class
+ **/
+class ExampleTestTest {
+    @Test
+    void name() {
+    }
+}
