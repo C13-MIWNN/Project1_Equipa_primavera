a8948ad51407b9d5b8df51dfb903576b80a554cf
mirjam schmitz <mirjam_schmitz@hotmail.com>
2024-04-21 21:15:43 +0200

    test idea, but how?

--- src/test/java/nl/miwnn/se13/equipaprimavera/ReceitaDePrimavera/ExampleTest.java
+++ src/test/java/nl/miwnn/se13/equipaprimavera/ReceitaDePrimavera/ExampleTest.java
@@ -1,26 +1,39 @@
 package nl.miwnn.se13.equipaprimavera.ReceitaDePrimavera;
 
+import nl.miwnn.se13.equipaprimavera.ReceitaDePrimavera.model.Recipe;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.awt.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @author Mirjam Schmitz
  * <p>
- * Purpose for the class
+ * Test
  **/
 class ExampleTest {
     @Test
     void shouldShowSimpleAssertion() {
-        Assertions.assertEquals(1,1);
+        assertEquals(1,1);
     }
 
-    @Test
-    @DisplayName("shouldShow")
-    void shouldShow() {
-        
-        org.junit.jupiter.api.Assertions.fail("Not implemented");
-    }
+
+//    @Test // kan je testen of het aantal en/of de namen van de toegevoegde recipes in een recipebook kloppen?
+//    @DisplayName("Should check all items in the list")
+//    void shouldCheckAllRecipesInTheRecipeBook() {
+//        List<Recipe> recipeList = List.of(, rec, 5, 7);
+//
+//        Assertions.assertAll(() -> assertEquals(2, recipeList.get(0)),
+//                () -> assertEquals(, recipeList.get(1)),
+//                () -> assertEquals(, recipeList.get(2)),
+//                () -> assertEquals(, recipeList.get(3)));
+//    }
+
 }
